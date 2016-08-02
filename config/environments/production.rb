c1139875f28cb07176b33099b5f8d6626d52e60a
Ivan <icwong@sfu.ca>
2016-08-02 18:23:05 +0000

    adasd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'cooprs.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'salty-garden-19115.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
